ad9c795b50dab22608f14eb3189ba360ace5c277
Adeyemi Glory <adeyemiglr@gmail.com>
2022-04-26 00:12:58 +0100

    updated

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const adminsRoute = require("./routes/admins");
 const app = express();
 app.use(cors());
 env.config();
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 // app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
